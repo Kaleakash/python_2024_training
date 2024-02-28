0231c9c5bad461819616436deeee05d0d9662344
akash <akash300383@gmail.com>
2024-02-28 14:47:29 +0530

    done

---
+++ Python Programs/dicDemo.py
@@ -0,0 +1,22 @@
+dict1  = {};
+dict2=dict();
+print(dict1);
+print(dict2);
+print(type(dict1));
+print(type(dict2));
+std_dict= {"100":"Ravi","101":"Steven","102":"Lex","102":"Leena","103":"Ravi"}
+print(std_dict);
+print(std_dict["100"]); # no return it generate error else it return value 
+print(std_dict.get("1000"));    # if no record it return Null else it return value
+print(std_dict.keys());     # display all keys as string format 
+print(std_dict.values());   # display all values as string format 
+for key in std_dict.keys():
+    print(key);
+for value in std_dict.values():
+    print(value);
+for data in std_dict.items():
+    print(data);
+print(dir(std_dict));
+
+    
+    
